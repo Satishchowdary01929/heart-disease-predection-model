63edc6abd6202ba3acbf55a4f97963bb65a46e96
Satishchowdary01929 <132988307+Satishchowdary01929@users.noreply.github.com>
2024-09-17 20:02:13 +0530

    Add files via upload

---
+++ import numpy as np77.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+import os
+print(os.listdir())
+
+import warnings
+warnings.filterwarnings('ignore')
+
+dataset = pd.read_csv("heart.csv")
+
+type(dataset)
+
+dataset.shape
+
+dataset.head(5)
+
+
+from sklearn.model_selection import train_test_split
+
+predictors = dataset.drop("target",axis=1)
+target = dataset["target"]
+
+X_train,X_test,Y_train,Y_test = train_test_split(predictors,target,test_size=0.20,random_state=0)
+X_train.shape
+X_test.shape
+Y_train.shape
+Y_test.shape
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+
+lr = LogisticRegression()
+
+lr.fit(X_train,Y_train)
+
+Y_pred_lr = lr.predict(X_test)
+
+Y_pred_lr.shape
+score_lr = round(accuracy_score(Y_pred_lr,Y_test)*100,2)
+
+print("The accuracy score achieved using Logistic Regression is: "+str(score_lr)+" %")
